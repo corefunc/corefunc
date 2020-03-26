0df869fe7befe173ef3b20c989166d5e1824f681
Anton Trofimenko <r37r0m0d3l@users.noreply.github.com>
2020-03-26 15:33:35 +0200

    Added: checkIsFunctionArrow.

---
+++ check/isFunctionArrow.mjs
@@ -0,0 +1,41 @@
+export default function checkIsFunctionArrow(callable) {
+  if (!callable || typeof callable !== "function") {
+    return false;
+  }
+  const source = [...callable.toString()];
+  let index = 0;
+  let countParen = 0;
+  let countBracket = 0;
+  let char;
+  let charLast;
+  let charFirst;
+  while (index < source.length) {
+    char = source[index];
+    if (
+      char === "=" &&
+      countParen === 0 &&
+      countBracket === 0 &&
+      index + 1 < source.length &&
+      source[index + 1] === ">"
+    ) {
+      return true;
+    } else if (char === "(" && !charFirst) {
+      countParen++;
+    } else if (char === ")" && !charFirst) {
+      countParen--;
+    } else if (char === "{" && !charFirst) {
+      countBracket++;
+    } else if (char === "}" && !charFirst) {
+      countBracket--;
+    } else if (char === '"' || char === "'" || char === "`") {
+      if (charFirst && char === charFirst && charLast !== "\\") {
+        charFirst = undefined;
+      } else if (!charFirst) {
+        charFirst = char;
+      }
+    }
+    charLast = char;
+    index++;
+  }
+  return false;
+}
---
+++ check/isFunctionArrow.ts
@@ -0,0 +1,41 @@
+export default function checkIsFunctionArrow<F extends Function>(callable: F): boolean {
+  if (!callable || typeof callable !== "function") {
+    return false;
+  }
+  const source: string[] = [...callable.toString()];
+  let index: number = 0;
+  let countParen: number = 0;
+  let countBracket: number = 0;
+  let char: string;
+  let charLast: string;
+  let charFirst: string;
+  while (index < source.length) {
+    char = source[index];
+    if (
+      char === "=" &&
+      countParen === 0 &&
+      countBracket === 0 &&
+      index + 1 < source.length &&
+      source[index + 1] === ">"
+    ) {
+      return true;
+    } else if (char === "(" && !charFirst) {
+      countParen++;
+    } else if (char === ")" && !charFirst) {
+      countParen--;
+    } else if (char === "{" && !charFirst) {
+      countBracket++;
+    } else if (char === "}" && !charFirst) {
+      countBracket--;
+    } else if (char === '"' || char === "'" || char === "`") {
+      if (charFirst && char === charFirst && charLast !== "\\") {
+        charFirst = undefined;
+      } else if (!charFirst) {
+        charFirst = char;
+      }
+    }
+    charLast = char;
+    index++;
+  }
+  return false;
+}
