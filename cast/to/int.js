c6d1fcd594d7162708ede1b4575c8d5356c7eb2d
Anton Trofimenko <r37r0m0d3l@users.noreply.github.com>
2020-04-10 15:52:03 +0300

    Updated dependencies.

---
+++ cast/to/int.js
@@ -0,0 +1,25 @@
+/**
+ * Typecast variable to integer
+ * @param {*}      variable
+ * @param {*}      [onFail=0] The return value in case of failure
+ * @param {Number} [radix=10] An integer that represents the radix of the above mentioned string
+ * @returns {Number}
+ * @example castToInt("F", 0, 16); // => 15
+ */
+module.exports = function castToInt(variable, onFail = 0, radix = 10) {
+  const type = typeof variable;
+  if (type === "boolean") {
+    return Number(variable);
+  }
+  if (type === "string") {
+    const temporary = Number.parseInt(variable, radix);
+    if (Number.isNaN(temporary) || !Number.isFinite(temporary)) {
+      return onFail;
+    }
+    return temporary;
+  }
+  if (type === "number" && Number.isFinite(variable)) {
+    return variable | 0;
+  }
+  return onFail;
+};
