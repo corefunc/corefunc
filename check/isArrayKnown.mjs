37b847b72b4039c17307a45c5f7201fbd585895d
Anton Trofimenko <r37r0m0d3l@users.noreply.github.com>
2020-04-29 17:01:42 +0300

    isArrayKnown

---
+++ check/isArrayKnown.mjs
@@ -0,0 +1,21 @@
+/**
+ * @name isArrayKnown
+ * @param {Array} array
+ * @returns {boolean}
+ * @example
+ * const arrayOfStrings = ["A", "B", "C"];
+ * const arrayOfNumbers = [1, 2, 3];
+ * isArrayKnown<number[]>(arrayOfStrings);
+ * // TS2345: Argument of type 'string[]' is not assignable to parameter of type 'number[]'.
+ * // Type 'string' is not assignable to type 'number'.
+ * isArrayKnown<string[]>(arrayOfStrings);
+ * // OK
+ * isArrayKnown<number[]>(arrayOfNumbers);
+ * // OK
+ * isArrayKnown<string[]>(arrayOfNumbers);
+ * // TS2345: Argument of type 'number[]' is not assignable to parameter of type 'string[]'.
+ * //   Type 'number' is not assignable to type 'string'.
+ */
+export default function isArrayKnown(array) {
+  return Array.isArray(array);
+}
