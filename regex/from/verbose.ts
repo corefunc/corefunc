dd5736a52187ebf8bd22ff251bb5e0ccfd697a32
Anton Trofimenko <r37r0m0d3l@users.noreply.github.com>
2020-05-03 19:16:16 +0300

    Added: regexFindNumbers, regexFromVerbose.

---
+++ regex/from/verbose.ts
@@ -0,0 +1,29 @@
+/**
+ * @name regexFromVerbose
+ * @param {TemplateStringsArray} input
+ * @returns {RegExp}
+ * @example
+ * new RegExp(
+ *   regexFromVerbose`
+ *     (?<!\\) \s             // Ignore whitespace, but not
+ *                            // when escaped with backslash.
+ *   | [/][/] .*              // Single-line comment.
+ *   | [/][*] [\s\S]* [*][/]  // Multi-line comment.
+ *                            // Note: /[\s\S]/ is same as /./s
+ *                            // with dot-all flag (s).
+ * `,
+ *   "g",
+ * );
+ * const regExp = regexFromVerbose`
+ *   (0 | [1-9] [0-9]*)   // Integer part with no leading zeroes
+ *   \.                   // Dot
+ *   [0-9]*               // Fractional part (optional)
+ *   ([eE] [+-]? [0-9]+)? // Exponent part (optional)
+ * `;
+ */
+export function regexFromVerbose(input: TemplateStringsArray) {
+  if (input.raw.length !== 1) {
+    throw Error("regexFromVerbose: interpolation is not supported");
+  }
+  return new RegExp(input.raw[0].replace(/(?<!\\)\s|[/][/].*|[/][*][\s\S]*[*][/]/g, ""));
+}
