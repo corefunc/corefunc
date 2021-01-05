ce4ccd7d1e56a77c7e5b3f9c80510b209fcd19cd
Anton Trofimenko <r37r0m0d3l@protonmail.com>
2021-01-05 13:51:12 +0200

    feat: add missing `/v8/clone.js`

---
+++ v8/clone.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.v8Clone = void 0;
+const v8 = require("v8");
+/**
+ * @name v8Clone
+ * @param {*} obj
+ * @returns {*}
+ */
+function v8Clone(obj) {
+  return v8.deserialize(v8.serialize(obj));
+}
+exports.v8Clone = v8Clone;
