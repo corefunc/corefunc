d650e6fa2f4415b3e1164dca9eb769b381684921
Anton Trofimenko <r37r0m0d3l@protonmail.com>
2021-05-07 18:16:05 +0300

    feat(convert): refactor `convertMapToObject`

--- convert/map/object.js
+++ convert/map/object.js
@@ -2,34 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.convertMapToObject = void 0;
 /**
- * @deprecated
- * @param map
+ * @category Convert Map
+ * @name convertMapToObject
+ * @description Convert Map to plain object.
+ * @summary ```import { convertMapToObject } from '@corefunc/corefunc/convert/map/object';```
+ * @param {Map} [map] Map to convert.
+ * @returns {Object} Plain object.
  */
 function convertMapToObject(map) {
-    const object = {};
-    map.forEach((value, key) => {
-        switch (typeof value) {
-            case "string":
-                object[key] = value;
-                break;
-            case "number":
-                {
-                    const numericKey = value.toString();
-                    if (!map.has(numericKey)) {
-                        object[numericKey] = value;
-                    }
-                }
-                break;
-            case "boolean":
-                {
-                    const booleanKey = value.toString();
-                    if (!map.has(booleanKey)) {
-                        object[booleanKey] = value;
-                    }
-                }
-                break;
-        }
-    });
-    return object;
+    if (!map || !(map instanceof Map)) {
+        return {};
+    }
+    return Object.fromEntries(map);
 }
 exports.convertMapToObject = convertMapToObject;
--- convert/map/object.mjs
+++ convert/map/object.mjs
@@ -1,31 +1,14 @@
 /**
- * @deprecated
- * @param map
+ * @category Convert Map
+ * @name convertMapToObject
+ * @description Convert Map to plain object.
+ * @summary ```import { convertMapToObject } from '@corefunc/corefunc/convert/map/object';```
+ * @param {Map} [map] Map to convert.
+ * @returns {Object} Plain object.
  */
 export function convertMapToObject(map) {
-  const object = {};
-  map.forEach((value, key) => {
-    switch (typeof value) {
-      case "string":
-        object[key] = value;
-        break;
-      case "number":
-        {
-          const numericKey = value.toString();
-          if (!map.has(numericKey)) {
-            object[numericKey] = value;
-          }
-        }
-        break;
-      case "boolean":
-        {
-          const booleanKey = value.toString();
-          if (!map.has(booleanKey)) {
-            object[booleanKey] = value;
-          }
-        }
-        break;
-    }
-  });
-  return object;
+  if (!map || !(map instanceof Map)) {
+    return {};
+  }
+  return Object.fromEntries(map);
 }
