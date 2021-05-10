7f61affba0f6a4dccd7f6131cbbe420f25c1794f
Anton Trofimenko <r37r0m0d3l@protonmail.com>
2021-05-10 23:09:27 +0300

    feat: added `textFitLength`

---
+++ text/fit/length.mjs
@@ -0,0 +1,31 @@
+/**
+ * @category Text Fit
+ * @name textFitLength
+ * @description Trim and fit text to length
+ * @summary ```import { textFitLength } from '@corefunc/corefunc/text/fit/length';```
+ * @param {string} [text] Text to fit.
+ * @param {number} [length=99] Length in character count, not string length.
+ * @param {boolean} [addEllipsis=true] Add ellipsis if text was cut.
+ * @returns {string} New text.
+ * @example ```textFitLength("Hello, world!", 6) ➜ "Hello…"```
+ * @since 0.1.69
+ */
+export function textFitLength(text, length = 99, addEllipsis  = true) {
+  if (length < 1) {
+    return "";
+  }
+  let chars = Array.from(String(text).normalize().trim());
+  if (chars.length === 0) {
+    return "";
+  }
+  if (chars.length <= length) {
+    return chars.join("");
+  }
+  if (addEllipsis) {
+    chars = chars.slice(0, length - 1);
+    chars.push("…");
+  } else {
+    chars = chars.slice(0, length);
+  }
+  return chars.join("");
+}
