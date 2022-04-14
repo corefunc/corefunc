479d6da38b0a7443f9d5fede0af0051ccb2aeeb1
Anton Trofimenko <r37r0m0d3l@protonmail.com>
2022-04-14 16:11:41 +0300

    feat: update `textCaseSnake`

--- text/case/snake.js
+++ text/case/snake.js
@@ -5,11 +5,16 @@ exports.textCaseSnake = void 0;
  * @category Text Case
  * @name textCaseSnake
  * @description Converts string into snake-case.
+ * @summary ```import { regexMatch } from '@corefunc/corefunc/text/case/snake';```
  * @param {string} text
  * @returns {string}
  * @see https://regex101.com/r/QeSm2I/1
+ * @since 0.3.33
  */
 function textCaseSnake(text) {
-    return text.replace(/(?:([a-z])([A-Z]))|(?:((?!^)[A-Z])([a-z]))/g, "$1_$3$2$4").toLowerCase();
+    return text
+        .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map((txt) => txt.toLowerCase())
+        .join("_");
 }
 exports.textCaseSnake = textCaseSnake;
--- text/case/snake.mjs
+++ text/case/snake.mjs
@@ -2,10 +2,15 @@
  * @category Text Case
  * @name textCaseSnake
  * @description Converts string into snake-case.
+ * @summary ```import { regexMatch } from '@corefunc/corefunc/text/case/snake';```
  * @param {string} text
  * @returns {string}
  * @see https://regex101.com/r/QeSm2I/1
+ * @since 0.3.33
  */
 export function textCaseSnake(text) {
-  return text.replace(/(?:([a-z])([A-Z]))|(?:((?!^)[A-Z])([a-z]))/g, "$1_$3$2$4").toLowerCase();
+  return text
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map((txt) => txt.toLowerCase())
+    .join("_");
 }
